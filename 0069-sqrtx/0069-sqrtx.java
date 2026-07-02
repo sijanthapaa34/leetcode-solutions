d6ff37559704663a16911508eff6984189f57419
Sijan Thapa <sijanmgr034@gmail.com>
2026-07-02 10:30:07 +0545

    Time: 1 ms (97.64%), Space: 42.6 MB (42.36%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+
+        int left = 1;
+        int right = x;
+        int idx = 0;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(mid > x/mid){
+                right = mid-1;
+            }else{
+                idx = mid;
+                left = mid+1;
+            }
+        }
+        return idx;
+    }
+}
