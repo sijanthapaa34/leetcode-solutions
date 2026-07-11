31b55f6be954c970e36c11db7d93a84863a03035
Sijan Thapa <sijanmgr034@gmail.com>
2026-07-11 10:24:45 +0545

    Time: 1 ms (99.98%), Space: 42.6 MB (45.85%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,35 +3,57 @@ class Solution {
         boolean isNeg = x<0;
         long n = Math.abs(x);
         
-        // String str = String.valueOf(Math.abs(x));
-        // char[] chars = str.toCharArray();
-        // StringBuilder sb = new StringBuilder();
+        /* Method 1
+        String str = String.valueOf(Math.abs(x));
+        char[] chars = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
 
-        // for(int i = 0; i<chars.length; i++){
-        //     sb.append(chars[chars.length-i-1]);
+        for(int i = 0; i<chars.length; i++){
+            sb.append(chars[chars.length-i-1]);
 
-        // }
-        // long result = Long.parseLong(sb.toString());
+        }
+        long result = Long.parseLong(sb.toString());
 
-        // if(isNeg)
-        //     result = -result;
-        // if(Integer.MIN_VALUE>result || result>Integer.MAX_VALUE)
-        //     return 0;
+        if(isNeg)
+            result = -result;
+        if(Integer.MIN_VALUE>result || result>Integer.MAX_VALUE)
+            return 0;
         
-        // return (int) result;
+        return (int) result; 
+        */
 
+        /* Method 2
         long newDigit = 0;
         while(n>0){
             newDigit *= 10;
             newDigit += n%10;
+            // newDigit = newDigit*10 + n%10;
+
             n /= 10;
         }
 
         if(isNeg) newDigit = -newDigit;
-
+        
         if (newDigit > Integer.MAX_VALUE || newDigit < Integer.MIN_VALUE)
             return 0;
 
         return (int) newDigit;
+         */
+
+        long newDigit = doReverse(0, n);
+
+        if(isNeg) newDigit = -newDigit;
+        
+        if (newDigit > Integer.MAX_VALUE || newDigit < Integer.MIN_VALUE)
+            return 0;
+
+        return (int) newDigit;
+
+    }
+    public long doReverse(long newDigit, long n) {
+        if(n == 0){
+            return newDigit;
+        }
+        return doReverse(newDigit*10 + n%10, n/10);
     }
 }
